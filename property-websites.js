c24491bffef95aadc9be4564c605fd5ef065255d
Stefany Serino <stefany.serino@siteminder.com>
2026-01-16 13:50:01 +1100

    add query to get property data for reference

--- property-websites.js
+++ property-websites.js
@@ -19,3 +19,34 @@ module.exports = (() => {
 
   return spidWebsiteMap
 })();
+
+// Databricks query for reference
+// https://siteminder-pciprod.cloud.databricks.com/editor/queries/4166014845012662?o=7316601961078759
+/*
+select region,
+       id,
+       replace(channel_code, '\n', '\\n') as channel_code,
+       siteminder_property_id,
+       replace(name, '\n', '\\n') as name,
+       partner, group, payment_gateway_name,
+       replace(home_page_link, '\n', '\\n') as home_page_link,
+       custom_domain,
+       deleted,
+       suspended
+FROM (
+  SELECT 'apac' region, p.siteminder_property_id, w.name as partner, g.name as group, b.channel_code, p.*
+  FROM pciprod_prod_views.`tbb_core-apac`.properties p
+  LEFT JOIN pciprod_prod_views.`tbb_core-apac`.groups g ON g.id = p.group_id
+  LEFT JOIN pciprod_prod_views.`tbb_core-apac`.partners w ON w.id = g.partner_id
+  LEFT JOIN pciprod_prod_views.`tbb_core-apac`.booking_channels b ON b.property_id = p.id
+  UNION ALL
+  SELECT 'emea' region, p.siteminder_property_id, w.name as partner, g.name as group, b.channel_code, p.*
+  FROM pciprod_prod_views.`tbb_core-emea`.properties p
+  LEFT JOIN pciprod_prod_views.`tbb_core-emea`.groups g ON g.id = p.group_id
+  LEFT JOIN pciprod_prod_views.`tbb_core-emea`.partners w ON w.id = g.partner_id
+  LEFT JOIN pciprod_prod_views.`tbb_core-emea`.booking_channels b ON b.property_id = p.id
+) merged_properties
+where siteminder_property_id IN (
+ ...list of SPIDs...
+);
+*/
